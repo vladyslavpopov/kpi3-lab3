48cc9b2fdd81d22159d792d65f49c7ec0f3c59c7
Roman Mazur <roman@rmazur.io>
2023-04-21 23:31:36 +0200

    Clean up go.mod

--- painter/loop.go
+++ painter/loop.go
@@ -2,11 +2,12 @@ package painter
 
 import (
 	"image"
+	"sync"
 
 	"golang.org/x/exp/shiny/screen"
 )
 
-// Receiver отримує текстуру, яка була підготовлена в результаті виконання команд у циелі подій.
+// Receiver отримує текстуру, яка була підготовлена в результаті виконання команд у циклі подій.
 type Receiver interface {
 	Update(t screen.Texture)
 }
@@ -16,9 +17,12 @@ type Loop struct {
 	Receiver Receiver
 
 	next screen.Texture // текстура, яка зараз формується
-	prev screen.Texture // текстура, яка була відправленя останнього разу у Receiver
+	prev screen.Texture // текстура, яка була відправлення останнього разу у Receiver
 
 	mq messageQueue
+
+	stop    chan struct{}
+	stopReq bool
 }
 
 var size = image.Pt(400, 400)
@@ -28,29 +32,71 @@ func (l *Loop) Start(s screen.Screen) {
 	l.next, _ = s.NewTexture(size)
 	l.prev, _ = s.NewTexture(size)
 
-	// TODO: ініціалізувати чергу подій.
-	// TODO: запустити рутину обробки повідомлень у черзі подій.
+	l.stop = make(chan struct{})
+
+	go func() {
+		for !l.stopReq || !l.mq.empty() {
+			op := l.mq.pull()
+			if update := op.Do(l.next); update {
+				l.Receiver.Update(l.next)
+				l.next, l.prev = l.prev, l.next
+			}
+		}
+		close(l.stop)
+	}()
 }
 
 // Post додає нову операцію у внутрішню чергу.
 func (l *Loop) Post(op Operation) {
-	// TODO: реалізувати додавання операції в чергу. Поточна імплементація
-	update := op.Do(l.next)
-	if update {
-		l.Receiver.Update(l.next)
-		l.next, l.prev = l.prev, l.next
+	l.mq.push(op)
+}
+
+// StopAndWait сигналізує про необхідність завершити цикл та блокується до моменту його повної зупинки.
+func (l *Loop) StopAndWait() {
+	l.Post(OperationFunc(func(screen.Texture) {
+		l.stopReq = true
+	}))
+	<-l.stop
+}
+
+type messageQueue struct {
+	ops     []Operation
+	mu      sync.Mutex
+	blocked chan struct{}
+}
+
+func (mq *messageQueue) push(op Operation) {
+	mq.mu.Lock()
+	defer mq.mu.Unlock()
+
+	mq.ops = append(mq.ops, op)
+
+	if mq.blocked != nil {
+		close(mq.blocked)
+		mq.blocked = nil
 	}
 }
 
-// StopAndWait сигналізує
-func (l *Loop) StopAndWait() {
+func (mq *messageQueue) pull() Operation {
+	mq.mu.Lock()
+	defer mq.mu.Unlock()
 
+	for len(mq.ops) == 0 {
+		mq.blocked = make(chan struct{})
+		mq.mu.Unlock()
+		<-mq.blocked
+		mq.mu.Lock()
+	}
+
+	op := mq.ops[0]
+	mq.ops[0] = nil
+	mq.ops = mq.ops[1:]
+	return op
 }
 
-// TODO: реалізувати власну чергу повідомлень.
-type messageQueue struct {
+func (mq *messageQueue) empty() bool {
+	mq.mu.Lock()
+	defer mq.mu.Unlock()
+
+	return len(mq.ops) == 0
 }
-
-func (mq *messageQueue) push(op Operation) {}
-
-func (mq *messageQueue) pull() Operation { return nil }
