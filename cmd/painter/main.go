ec2d048fe09c65acaa8dcb564dbbe4c673648954
Vladyslav Popov <popovvladislav4891@gmail.com>
2025-05-29 21:34:10 +0300

    Initialize visualizer & HTTP command loop

--- cmd/painter/main.go
+++ cmd/painter/main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"net/http"
 
-	"github.com/roman-mazur/architecture-lab-3/painter"
-	"github.com/roman-mazur/architecture-lab-3/painter/lang"
-	"github.com/roman-mazur/architecture-lab-3/ui"
+	"github.com/vladyslavpopov/kpi3-lab3/painter"
+	"github.com/vladyslavpopov/kpi3-lab3/painter/lang"
+	"github.com/vladyslavpopov/kpi3-lab3/ui"
 )
 
 func main() {
